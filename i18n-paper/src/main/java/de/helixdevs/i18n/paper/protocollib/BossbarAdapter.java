77e6ab70f158b7eac9795f0792c81818fb792a7a
MCMDEV <john-m.1@gmx.de>
2022-08-14 21:23:23 +0200

    Fixed BossbarAdapter NPE

--- i18n-paper/src/main/java/de/helixdevs/i18n/paper/protocollib/BossbarAdapter.java
+++ i18n-paper/src/main/java/de/helixdevs/i18n/paper/protocollib/BossbarAdapter.java
@@ -4,6 +4,7 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import org.bukkit.plugin.Plugin;
 
 public class BossbarAdapter extends PacketAdapter implements ITranslationAdapter {
@@ -16,6 +17,8 @@ public class BossbarAdapter extends PacketAdapter implements ITranslationAdapter
     public void onPacketSending(PacketEvent event) {
         PacketContainer packet = event.getPacket();
         packet.getStructures().modify(1, internalStructure -> {
+            WrappedChatComponent wrappedChatComponent = internalStructure.getChatComponents().readSafely(0);
+            if(wrappedChatComponent == null) return internalStructure;
             handleChatComponent(event.getPlayer(), internalStructure, 0);
             return internalStructure;
         });
